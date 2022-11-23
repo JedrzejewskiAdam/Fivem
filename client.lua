f3b50340bcfcf0aba8427f4e0eb50b5ee6379b41
JedrzejewskiAdam <113061917+JedrzejewskiAdam@users.noreply.github.com>
2022-11-23 17:08:35 +0100

    Delete client.lua

--- client.lua
+++
@@ -1,5 +0,0 @@
-RegisterNetEvent('natywka:1')
-AddEventHandler('natywka:1', function()
-    local user = PlayerPedId()
-    SetEntityCoords(user, 338.7400, -584.1171, 74.1617, false, false, false, true)
-end)
