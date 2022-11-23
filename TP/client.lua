da601371b540082321523a815557eac5fb02341f
JedrzejewskiAdam <113061917+JedrzejewskiAdam@users.noreply.github.com>
2022-11-23 17:07:25 +0100

    Create client.lua
    Dołączenie nowego pliku :) [client.lua]

---
+++ TP/client.lua
@@ -0,0 +1,5 @@
+RegisterNetEvent('natywka:1')
+AddEventHandler('natywka:1', function()
+    local user = PlayerPedId()
+    SetEntityCoords(user, 338.7400, -584.1171, 74.1617, false, false, false, true)
+end)
